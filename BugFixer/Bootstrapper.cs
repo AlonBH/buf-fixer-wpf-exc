e0d8b79db42bc5c393127314159682d82c1a6c41
Alon Ben Haim <alon.b.h.672@gmail.com>
2020-03-21 22:19:09 +0200

    feat(fix-bugs): added the ability to fix any bug

---
+++ BugFixer/Bootstrapper.cs
@@ -0,0 +1,19 @@
+using System.Windows;
+using BugFixer.ViewModels;
+using Caliburn.Micro;
+
+namespace BugFixer
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ BugFixer/ViewModels/ShellViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+
+namespace BugFixer.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        public ShellViewModel()
+        {
+            FixedBugs = 0;
+            MAX_BUGS = 30;
+        }
+        
+        private int MAX_BUGS;
+
+        private int _fixedBugs;
+
+		public int FixedBugs
+		{
+            get
+            {
+                return _fixedBugs;
+            }
+            set
+            {
+                _fixedBugs = value;
+                NotifyOfPropertyChange(() => FixedBugs);
+            }
+		}
+
+        public bool CanFixBug(int fixedBugs)
+        {
+            return fixedBugs < MAX_BUGS;
+        }
+
+        public void FixBug(int fixedBugs)
+        {
+            FixedBugs++;
+        }
+
+	}
+}
